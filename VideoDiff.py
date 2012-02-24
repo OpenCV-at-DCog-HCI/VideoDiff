930a8c711ee423eb5146becc1c6f4c2917b95eb5
Daniel Frysinger <daniel@frysinger.net>
2012-02-24 11:43:50 -0800

    Adding SURF feature tracking

--- VideoDiff.py
+++ VideoDiff.py
@@ -24,17 +24,29 @@ while 1:
     frameDiff = cv2.absdiff(lastFrame, frame)
     greyDiff = cv2.cvtColor(frameDiff, code=cv2.COLOR_BGR2GRAY)
 
-    retval, motionMask = cv2.threshold(greyDiff,10,1,cv2.THRESH_BINARY)
+    retval, motionMask = cv2.threshold(greyDiff,20,1,cv2.THRESH_BINARY)
 
     timestamp = time.clock()
     cv2.updateMotionHistory(motionMask, motionHistory, timestamp, 0.5)
     mg_mask, mg_orient = cv2.calcMotionGradient( motionHistory, 0.25, 0.05, apertureSize=5 )
     seg_mask, seg_bounds = cv2.segmentMotion(motionHistory, timestamp, 0.25)
 
-    total = sum(sum(motionHistory))/8
+    surf = cv2.SURF()
+    grey = cv2.cvtColor(frame, code=cv2.COLOR_BGR2GRAY)
+    mask = numpy.uint8(numpy.ones(grey.shape))
+    points = surf.detect(grey, mask)
+
+    vis = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
+
+    for p in points[::10]:
+        cv2.circle(vis,(int(p.pt[0]),int(p.pt[1])), 2, (0,255,0),-1)
+        cv2.circle(vis,(int(p.pt[0]),int(p.pt[1])),int(p.size),(0,255,0),2)
+
+
+    total = sum(sum(motionHistory))/8*255
     print "movement: ", total
 
-    cv2.imshow("My Window", motionHistory)
+    cv2.imshow("My Window", vis)
     cv2.waitKey(1)
 
     lastFrame = frame.copy()
